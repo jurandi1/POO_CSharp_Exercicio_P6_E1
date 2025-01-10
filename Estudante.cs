43897f3ae28ba7328ce7563c94fcd157a8d962cb
Jurandi Junior <jurandijr20@gmail.com>
2025-01-09 21:47:08 -0300

    Exercício concluído

---
+++ Estudante.cs
@@ -0,0 +1,17 @@
+﻿namespace POO_CSharp_Exercicio_P6_E1
+{
+    internal class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
